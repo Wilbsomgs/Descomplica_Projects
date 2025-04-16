8b870c9688d001d1072726cf7649c8c36bbbae80
Wilbsom Gonçalves da Silva <151963771+Wilbsomgs@users.noreply.github.com>
2025-04-15 21:53:11 -0300

    Início projeto de sugestão de senhas fortes Java

---
+++ GrupoDeEstudos/SugestaoSenha.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SugestaoSenha {
+    public static void main (String[] args){
+        System.out.println("Funcionou");
+
+       // Definição de variáveis
+        String senhaUsuario;
+
+        Scanner senha = new Scanner(System.in);
+
+        System.out.println("Digite a senha: ");
+
+        senhaUsuario = senha.nextLine();
+
+        System.out.println(senhaUsuario);
+    
+
+       // Entrada de dados
+
+       // Senha invertida
+
+       // Senha em caixa alta
+
+       // Modo sem vogais
+
+       // Modo Cifrado
+       
+    }
+}
