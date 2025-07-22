7d15e20506bc1afd8af4b98d4c721e27eb0f465a
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-07-22 20:16:39 -0300

    Editando exemplo001

--- JavaScript/VariaveisEConstantes/exemplo01.js
+++ JavaScript/VariaveisEConstantes/exemplo01.js
@@ -1,13 +1,13 @@
 // Criando uma constante
-
 const externo = "Olá, eu sou uma constante global";
+console.log(externo); // Exibindo no console a constante externo;
 
 // Exemplo usando Var
 function exemploVar(){
     if (true){
         var mensagem = " Olá, Faculdade Descomplica, eu sou uma var...";
     }
-    console.log(mensagem);
+    console.log(mensagem); // Fora do escopo, mas funcionando 
 }
 
 // Chamanando da função exemploVar()
@@ -25,7 +25,7 @@ function exemploLet(){
 function exemploLetErro(){
     if(true) {
         let mensagem = " Olá, Faculdade Descomplica, eu sou um let...";
-        console.log(mensagem) // Para quebrar o código
+        console.log(mensagem) // Para ñ quebrar o código
     }
     // console.log(mensagem); Exibe erro (porque não está no mesmo escopo)
 }
