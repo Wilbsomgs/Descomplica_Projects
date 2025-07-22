c26fb602de00b878e8b52b710d5e9416de83c8c6
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-07-22 20:14:31 -0300

    pasta funcoes com exemplo01

--- JavaScript/VariaveisEConstantes/exemplo01.js
+++ JavaScript/VariaveisEConstantes/exemplo01.js
@@ -1,31 +1,33 @@
-// // Criando uma constante
+// Criando uma constante
 
-// const externo = "Olá, eu sou uma constante global";
+const externo = "Olá, eu sou uma constante global";
 
-// // Exemplo usando Var
-// function exemploVar(){
-//     if (true){
-//         var mensagem = " Olá, Faculdade Descomplica, eu sou uma var...";
-//     }
-//     console.log(mensagem);
-// }
+// Exemplo usando Var
+function exemploVar(){
+    if (true){
+        var mensagem = " Olá, Faculdade Descomplica, eu sou uma var...";
+    }
+    console.log(mensagem);
+}
 
-// // Chamanando da função exemploVar()
-// exemploVar();
+// Chamanando da função exemploVar()
+exemploVar();
 
-// function exemploLet(){
-//     if(true) {
-//         let mensagem = " Olá, Faculdade Descomplica, eu sou um let...";
-//         console.log(mensagem);
-//     }
-// }
+function exemploLet(){
+    if(true) {
+        let mensagem = " Olá, Faculdade Descomplica, eu sou um let...";
+        console.log(mensagem);
+    }
+    
+}
 
 // Erro usando let
 function exemploLetErro(){
     if(true) {
         let mensagem = " Olá, Faculdade Descomplica, eu sou um let...";
+        console.log(mensagem) // Para quebrar o código
     }
-    console.log(mensagem);
+    // console.log(mensagem); Exibe erro (porque não está no mesmo escopo)
 }
 
 // Chamando função exemploLetErro()
