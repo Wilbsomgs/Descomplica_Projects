fe601bb106ab0a2e259f509212a5c8e7e730fea0
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-07-23 19:54:06 -0300

    adicionando chamada de funcao

---
+++ JavaScript/funcoes/funcaoRegular/exemplo001.js
@@ -0,0 +1,14 @@
+function exemploFuncao(a,b){ // Como declarar função 
+    return a + b;
+}
+
+// Declarando e chamando função 
+let x = exemplo(2,4);
+
+function exemplo(a,b){
+    return a + b; // Função que retorna a soma dos parâmetros a e b
+}
+
+let resultado = exemplo(13,4);
+console.log("O resultado é:  " + resultado) //Forma 01 de exibir
+console.log("O resultado é: " +  exemplo(13,4)); // Forma 02 de exibir
