e169d46152e97313956f045abda975ae13abcdc4
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-07-27 16:43:37 -0300

    Document innerHTML

---
+++ JavaScript/tiposSaidasJS/documentWrite/exemplo001/index.js
@@ -0,0 +1,4 @@
+document.write("<h2>Exemplo usando document.write</h2>");
+document.write("<h3>Nome</h3>", "<p>Wilbsom Gonçalves da Silva</p>");
+document.write("<h3>Curso</h3>", "<p>Ciência da Computação</p>");
+document.write("<h3>Ano</h3>", "<p>2024</p>");
---
+++ JavaScript/tiposSaidasJS/innerHTML/ex001/index.js
@@ -0,0 +1,12 @@
+
+function mudarParagrafo(){
+    document.getElementById("meuParagrafo").innerHTML = "Novo paragráfo";
+}
+
+function mudarHistoria2(){
+    document.getElementById("p-02").innerHTML = "A málicia dos maliciosos";
+}
+
+function tarefaCompleta(){
+    document.getElementById("p-completo").innerHTML = "Parabéns você coompletou a atividade sobre innerHTML";
+}
