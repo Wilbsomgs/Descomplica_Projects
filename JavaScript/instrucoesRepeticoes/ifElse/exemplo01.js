55804249c6b3ec73ed490aa0a2cc227009e61359
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-08-04 19:34:10 -0300

    Add instrucoesRepeticoes -- ifElse

---
+++ JavaScript/instrucoesRepeticoes/ifElse/exemplo01.js
@@ -0,0 +1,9 @@
+// Decidindo se levar guarda chuva caso chova e não levar 
+let weatherForcast = "rainy";
+
+if(weatherForcast === "raiy"){
+    
+    console.log("Devemos levar o guarda chuva");
+} else {
+    console.log("NÂO devemos levar o guarda chuva");
+}
