36974b27baa4d472aa2223c11892a225a0d86a84
Wilbsom Gonçalves da Silva <willsuporte12@gmail.com>
2025-08-05 07:40:59 -0300

    Add atvd02.js For

---
+++ JavaScript/instrucoesRepeticoes/lacoRepeticao/for/atvd01.js
@@ -0,0 +1,6 @@
+// Crie um for que imprima os múltiplos de 3 entre 1 e 30.
+for(let i = 1; i <= 30; i++){
+   if(i % 3 === 0){
+    console.log(i);
+   }
+}
---
+++ JavaScript/instrucoesRepeticoes/switch/exemplo02.js
@@ -0,0 +1,25 @@
+// Exemplo 02 usando o switch
+// Exibe o dia da semana 
+
+let day = 4
+
+switch(day){ // palavavra-chave(condicao)
+    case 1:
+        console.log("Segunda-feira");
+        break; // Encerra o switch
+    case 2: 
+        console.log("Terça-feira");
+        break;
+    case 3:
+        console.log("Quarta-feira");
+        break; // Encerra o switch
+    case 4: 
+        console.log("Quinta-feira");
+        break;
+    case 5: 
+        console.log("Sexta-feira");
+        break;   
+    default: 
+        console.log("Dia inválido");    
+    
+}
